c0d6285a7e3a38d883c042554037f45bdb93d801
Alexander Kudryashov <a.kudryashov.85@gmail.com>
2018-10-31 00:25:27 +0400

    Fixed syntax bug

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -45,9 +45,6 @@ class Login extends React.Component {
 		try {
 			await mutate({
 				variables,
-					username: username.value,
-					password: password.value
-				},
 				update: (store, { data: { login: token } }) => {
 					const { exp, iat, ...user } = jwt.decode(token)
 					document.cookie = `token=${token}; expires=${new Date(exp * 1000)}`
@@ -62,7 +59,7 @@ class Login extends React.Component {
 		} catch (error) {
 			// Handle error
 			this.setState({ isLoading: false })
-			console.log(error.message)
+			alert(error.message)
 		}
 	}
 
